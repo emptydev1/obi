61bcbf96cedc2e4d834b4827dea5fea1c5e00c0b
emptydev1 <emptydev8@gmail.com>
2024-05-20 20:36:58 -0300

    changed one question

--- 2021/Júnior/Fase 01/idade.py
+++ 2021/Júnior/Fase 01/idade.py
@@ -4,7 +4,5 @@
 # Objetivo: determinar a idade de Camila apartir de 3 numeros inteiros
 # 1. Se Cibele nasceu antes de Camila e Celeste nasceu depois de Camila, então isso significa dizer que Camila está no centro (meio)
 # 2. Dito isso, basta retornar a segunda maior idade fornecida
-ages = [int(input()) for _ in range(3)] # obtendo os valores numéricos
-ages.sort() # organizando do maior para o menor
 
-print(ages[1]) # imprimindo o resultado
+print(sorted([int(input()) for _ in range(3)], reverse=True)[1]) # organizando os valores numéricos do maior para o menor e imprimindo a segunda maior idade
